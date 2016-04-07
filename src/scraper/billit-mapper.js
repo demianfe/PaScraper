e1bc3b8f48f477030bad3ee330c1434da691e2df
demianfe <demian@sinergetica.com>
2016-04-07 08:53:07 -0400

    billit mapper first working version

--- src/scraper/billit-mapper.js
+++ src/scraper/billit-mapper.js
@@ -1,7 +1,7 @@
 'use strict';
+import request from 'request-promise';
+import { getBills, countUniqueBills, getUniqueBills } from './mongo-client'
 
-import request from 'promise-request';
-import getUniqueBills from 'mongo-client'
 ;
 const host = 'http://localhost:8002';
 const secret_token = '75cc973db60d3e07beaa1630c4cb37ded228e5bb71853be068a573b1a2ee385379111f9b12847b285a7e2c2b2f918b2902f4edb04046319cf41148a642fa53d3';
@@ -10,10 +10,93 @@ const secret_token = '75cc973db60d3e07beaa1630c4cb37ded228e5bb71853be068a573b1a2
 let headers = {"Content-Type": "application/json",
                "X-CSRF-Token": secret_token};
 
+let options  = {
+    method: 'GET',
+    uri: host,
+    headers: headers,
+    json: true
+};
+
 let getChamber = (chamber) => {
     if (chamber === "CAMARA DE DIPUTADOS") return "C. Diputados";
     else if (chamber === "CAMARA DE SENADORES") return "Senado";
     return chamber;
+};
+
+let createBillObject = (p) => {
+    let bill = {};
+    bill.silpy_id = p.idProyecto;
+    //bill.uuid = p.idProyecto;
+    if ('acapite' in p){
+	bill.title = p['acapite'];
+    }if ('fechaIngresoExpediente' in p){
+	bill.creation_date = p['fechaIngresoExpediente'];
+    }if ('origenProyecto' in p){
+	bill.initial_chamber = getChamber(p.origenProyecto);
+	bill.source = p.iniciativa;
+    }if ('tipoProyecto' in p){
+	bill.abstract = p.tipoProyecto;
+    }if ('urgencia' in p){
+	bill.urgent = p.urgencia;
+    }if ('descripcionEtapa' in p){
+	bill.stage = p.descripcionEtapa;
+    }if ('descripcionSubEtapa' in p){
+	bill.sub_stage = p.descripcionSubEtapa;
+    }if ('estadoProyecto' in p){
+	bill.status = p.estadoProyecto;
+    }
+    bill.authors = [];
+    if ('autores' in p){
+	console.log();
+	for (let author of p.autores.autor){
+	    bill.authors.push(author.idParlamentario + ':'
+			      + author.nombres + ' ' + author.apellidos);
+	}
+    }
+    //tramitaciones 
+    bill.tramitaciones = [];    
+    if ('tramitaciones' in p){
+	for (let tramitacion of p.tramitaciones.tramite){
+	    let new_tramitacion = {};
+	    new_tramitacion.session = tramitacion.numeroSesion;
+	    new_tramitacion.date = tramitacion.fechaTramite;
+	    new_tramitacion.chamber = getChamber(tramitacion.camaraTramite);
+	    new_tramitacion.stage = tramitacion.descripcionEtapa;
+	    new_tramitacion.timeline_status = tramitacion.resultadoTramite;      
+	    bill.tramitaciones.push(new_tramitacion);
+	}
+    }
+    //TODO: documents
+    bill.documents = [];
+    if ('file' in p){
+        document = {};
+        //document.type = doc['type']; // :type => String
+        // document.number#, :type => String
+        // document.step = doc['']#, :type => String
+        // document.stage = doc['']#, :type => String
+        // document.chamber = doc['']#, :type => String
+        document.link = p.file;//#, :type => String
+	bill.documents.push(document);
+    }
+    
+    // //TODO: Directives
+    // bill.directives = [];
+    // if ('directives' in p){
+    //     if (p['directives']){
+    //         for (let d of p['directives']){
+    //             let directive = {};
+    //             if ('date' in d){
+    //                 directive.date = d['date'];//#, :type => DateTime
+    //             }if ('result' in d){
+    //                 directive.step = d['result'];// #, :type => String
+    //                 //directive.stage #, :type => String ?
+    //                 //directive.link #, :type => String ?
+    // 		}	 
+    //             bill.directives.append(directive.__dict__);
+    // 	    }
+    // 	}
+    // }
+    return bill;
 };
 
 let  updateOrCreate = () => {
@@ -37,136 +120,58 @@ let  updateOrCreate = () => {
     //         r = requests.post(host + '/bills', data=json.dumps(bill.__dict__))
 };
 
-let mapBillit = () => {
+let postProjects = (bill) =>{    
+    console.log("loading bill with uuid=", bill.idProyecto);
+    let billitObj = createBillObject(bill);
+    options.uri = host + '/bills';
+    options.body = billitObj;
+    return request(options).then( (response) => {
+	console.log('Creado exitosamente ');
+	//console.log(response);
+    }).catch( (error ) =>{
+	console.log('Error !');
+	console.trace(error);
+    });
+    //r = requests.post(host + '/bills', data=json.dumps(bill.__dict__))
+    //print "------------------------------------------------------------------------------  "
+    //print r.content
+};
 
+let mapBillit = () => {
     let offset = 50;
     let start = 0;
     let end = offset;
-    let total = 0; //get all projects and count
-
-    while (end < total){
-        if (total - end > offset){
-            end += offset;
-        }else{
-            end = total;
-	}
-	
-        // projects = mdb.projects.find()[start:end]
-        // post_projects(projects)
-        // print "-------------------------"
-        // print "start " + str(start)
-        // print "end " + str(end)
-        // print "diff " + str(total - end)
-        // print "-------------------------"
-        // start = end
-    }
-};
-    
-let createBillObject = (p) => {
-    let bill = {};
-    bill.scrap_id = p['id'];
-    if ('title' in p){
-        bill.title = p['title'];
-    }if ('entry_date' in p){
-        bill.creation_date = p['entry_date'];
-    }if ('info' in p){
-        let info = p['info'];
-        bill.uid = info['file'];
-        if ('origin' in info){
-            bill.initial_chamber = get_chamber(info['origin']);
-            bill.source = info['iniciativa'];
-            //if 'subject' in p['info']:
-            //    bill.title = p['info']['subject']
-        }if ('heading' in info){
-            bill.abstract = info['heading'];
-	}if ('importance'in info){
-            if (info['importance'] == "SIN URGENCIA"){
-                bill.urgent = 'Simple';
-            }else{
-                bill.urgent = info['importance'];
-	    }
-	}if ('stage' in p){
-            stage = p['stage'];
-        }if ('stage' in stage){
-            bill.stage = stage['stage'];
-	}if ('sub_stage' in stage){
-            bill.sub_stage = stage['sub_stage'];
-	}if ('status' in stage){
-            bill.status = stage['status'];
-            bill.authors = [];
-	}
-    }if ('authors' in p){
-        for (author of p['authors']){
-            bill.authors.append(str(author['id']) + ':'+author['name']);
-	}
-    }
-    //paperworks
-    bill.paperworks = [];
-    if ('paperworks' in p){
-        for (let paperwork of p['paperworks']){
-            let new_paperwork = {};
-            if ('session' in paperwork){
-		new_paperwork.session = paperwork['session'];
-            } if ('date' in paperwork){
-                new_paperwork.date = paperwork['date'];
-	    }if ('chamber' in paperwork){
-                new_paperwork.chamber = getChamber(paperwork['chamber']);
-            }if ('stage' in paperwork){
-                new_paperwork.stage = paperwork['stage'];
-            }if ('result' in paperwork){
-                if ('value'in paperwork['result']){
-                    new_paperwork.timeline_status = paperwork['result']['value'];
-                    bill.paperworks.append(new_paperwork.__dict__);
-		   }
-	    }
-	}
-    }
-    //documents
-    bill.documents = [];
-    if ('documents' in p){
-        for (let doc of p['documents']){
-            document = {};
-            if ('registration_date' in doc){
-                document.date = doc['registration_date'];//#, :type => DateTime
-	    }if ('type' in doc){
-                document.type = doc['type']; // :type => String
-                //TODO: generar link a documento
-                // document.number#, :type => String
-                // document.step = doc['']#, :type => String
-                // document.stage = doc['']#, :type => String
-                // document.chamber = doc['']#, :type => String
-                document.link = doc['name'];//#, :type => String
-	    }
-            bill.documents.append(document.__dict__);
-	}
-    }
-    //Directives
-    bill.directives = [];
-    if ('directives' in p){
-        if (p['directives']){
-            for (let d of p['directives']){
-                let directive = {};
-                if ('date' in d){
-                    directive.date = d['date'];//#, :type => DateTime
-                }if ('result' in d){
-                    directive.step = d['result'];// #, :type => String
-                    //directive.stage #, :type => String ?
-                    //directive.link #, :type => String ?
-		}	 
-                bill.directives.append(directive.__dict__);
-	    }
-	}
-    }
-    return bill;
+    countUniqueBills().then( (total) => {
+	console.log('total', total);
+	// while (end < total){
+	//     if (total - end > offset){
+	// 	end += offset;
+	//     }else{
+	// 	end = total;
+	//     }
+	console.log('about to call getBills');
+	let count = 0;
+	getBills().then( (bills) => {
+	    bills.reduce( (sequence, bill) => {
+		return sequence.then( () => {
+		    console.log("-------------------------");
+		    count +=1;
+		    console.log('count', count);
+		    console.log("-------------------------");
+		    return postProjects(bill);
+		    // 
+		    // console.log( "start ",start);
+		    // console.log("end ", end);
+		    // console.log( "diff ", total - end);
+		    // console.log("-------------------------");
+		    // start = end;
+		}).catch( (err) => {
+		    console.trace(err);
+		});
+	    }, Promise.resolve());
+	});
+	//	}
+    });
 };
 
-let postProjects = (projects) =>{
-    for(let p of projects){
-        console.log("loading bill with uuid= %s" %(p['id']));
-        let bill = createBillObject(p);
-	
-        //r = requests.post(host + '/bills', data=json.dumps(bill.__dict__))
-        //print "------------------------------------------------------------------------------  "
-        //print r.content
-    }
-};
+mapBillit();
