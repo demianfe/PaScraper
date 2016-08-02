2398bebfc14a9371880be89dca70f52d2ee3e810
Demian Florentin <demian@tedic.org>
2016-08-02 11:17:35 -0400

    run commands via npm

--- src/main.js
+++ src/main.js
@@ -13,60 +13,62 @@ let command = process.argv[2];//process.argv.slice(2, process.argv.length);
 let args = process.argv.slice(3, process.argv.length);
 
 let printHelp = () => {
-    console.log("Usage: ");
-    console.log("--crawl-sessions \t args: year, the year to crawl.");
-    console.log("--download-rtfs \t downlad rtf. Args: year, download files from that year.");
-    console.log("--parse-rtfs \t\t parses all dowonloaded rtfs and saves to monto db.");
-    console.log("--map-votaciones \t maps diputados and voting results to votacionespa.");
-    console.log("--get-congressmen \t downloads congressmen data.");
-    console.log("--download-bills \t\ Download all bills related to all congressmen \n \
+  console.log("Usage: ");
+  console.log("npm start -- <command>\n")
+  console.log("Possible commands are:");
+  console.log("crawl-sessions \t args: year, the year to crawl.");
+  console.log("download-rtfs \t downlad rtf. Args: year, download files from that year.");
+  console.log("parse-rtfs \t\t parses all dowonloaded rtfs and saves to monto db.");
+  console.log("map-votaciones \t maps diputados and voting results to votacionespa.");
+  console.log("get-congressmen \t downloads congressmen data.");
+  console.log("download-bills \t\ Download all bills related to all congressmen \n \
 \t\t\t within this parlamentary period.");
-    console.log("--download-bills-files \t\ Download files related to bills. \n\
-\t\t\t --download-bills should be executed before.");
-    console.log("--map-bills \t\t maps all bills to bill-it.");
-    console.log("--update-bills \t\t updates bills with status \"EN TRAMITE\"");
+  console.log("download-bills-files \t\ Download files related to bills. \n\
+\t\t\t download-bills should be executed before.");
+  console.log("map-bills \t\t maps all bills to bill-it.");
+  console.log("update-bills \t\t updates bills with status \"EN TRAMITE\"");
 };
 switch (command){
-case "--help":
-    printHelp();
-    console.log("--help \t\t\t prints this message.");
-    break;
-case "--crawl-sessions":
-    console.log("Crawling sessions");
-    //check that the year is passed
-    crawlSessions({anho: args[0]});
-    break;
-case "--download-rtfs":
-    console.log("Downloading rtfs");
-    downloadRTFs(args[0]);
-    break;
-case "--parse-rtfs":
-    parseRTFs();
-    break;
-case "--map-votaciones":
-    mapAllToVotaciones();
-    break;
-case "--get-congressmen":
-    getCongressmenData();
-    break;
-case "--download-bills":
-    getBillsRelatedData();
-    break;
-case "--download-bills-files":
-    if(args[0] == 'new'){
-	downloadBills(true);
-    }else{
-	downloadBills();
-    }    
-    break;
-case "--map-bills":
-    mapBillit();
-    break;
-case "--update-bills":
-    updateBills();
-    break;
+case "help":
+  printHelp();
+  console.log("--help \t\t\t prints this message.");
+  break;
+case "crawl-sessions":
+  console.log("Crawling sessions");
+  //check that the year is passed
+  crawlSessions({anho: args[0]});
+  break;
+case "download-rtfs":
+  console.log("Downloading rtfs");
+  downloadRTFs(args[0]);
+  break;
+case "parse-rtfs":
+  parseRTFs();
+  break;
+case "map-votaciones":
+  mapAllToVotaciones();
+  break;
+case "get-congressmen":
+  getCongressmenData();
+  break;
+case "download-bills":
+  getBillsRelatedData();
+  break;
+case "download-bills-files":
+  if(args[0] == 'new'){
+    downloadBills(true);
+  }else{
+    downloadBills();
+  }    
+  break;
+case "map-bills":
+  mapBillit();
+  break;
+case "update-bills":
+  updateBills();
+  break;
 default:
-    printHelp();
-    //console.log("if you don't know what you are doing just type --help");
+  printHelp();
+  //console.log("if you don't know what you are doing just type --help");
 }
 
